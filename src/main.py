62767b0567b8e82e8d9ef67b677259c382830d4d
Udo Bauer <knackwurstking.tux@gmail.com>
2024-02-04 09:46:08 +0100

    main.py: adding link (non-blocking socket server accept)

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     # TODO: main loop:
     #   - check/connect wifi
     #   - check motion
-    #   - listen for client (non blocking)
+    #   - listen for client (non blocking) - https://stackoverflow.com/questions/5308080/python-socket-accept-nonblocking
     #   - sleep a second
     pass
