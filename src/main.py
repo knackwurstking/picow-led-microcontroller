9d550d5b6a5fdf65c083fc095982cd3994d1fc3b
Udo Bauer <knackwurstking.tux@gmail.com>
2024-02-04 09:37:19 +0100

    update source and main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+
+if __name__ == "__main__":
+    # TODO: main loop:
+    #   - check/connect wifi
+    #   - check motion
+    #   - listen for client (non blocking)
+    #   - sleep a second
+    pass
