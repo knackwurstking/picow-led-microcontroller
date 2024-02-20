8b46b882af884fdde2bc61a4e9eb0b3320624ac5
Udo Bauer <knackwurstking.tux@gmail.com>
2024-02-20 14:45:55 +0100

    add configuration values for config commands

--- src/config.py
+++ src/config.py
@@ -21,3 +21,21 @@ END_BYTE = b"\n"
 
 SOCKET_TIMEOUT_SELECT: int | float | None = 0.25
 SOCKET_TIMEOUT_SEND: int | float | None = 0.5
+
+
+# color pins in order (ex.: [r, g, b, w])
+COLOR_PINS: list[int] = []
+
+# motion pin set to -1 will disable the motion sensor
+MOTION_PIN: int = -1
+
+COLOR_PWM_RANGE_MIN: min = 0
+COLOR_PWM_RANGE_MAX: min = 100
+
+SERVER_ADDRESS: str = ""
+
+# TODO: set/get config values (handler to run if a value changes)
+#   - color pins
+#   - motion pin
+#   - server address
+#   - color pwm range
