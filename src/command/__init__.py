9323aa9cb0a255af756d6b37776deb946ee1a424
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-05 10:33:23 +0100

    update COMMANDS

--- src/command/__init__.py
+++ src/command/__init__.py
@@ -16,7 +16,7 @@ TYPE_EVENTS = "event"
 GROUP_CONFIG = "config"
 GROUP_INFO = "info"
 GROUP_LED = "led"
-GROUP_motion = "motion"
+GROUP_MOTION = "motion"
 
 
 @dataclass
@@ -28,26 +28,22 @@ class Command:
     def run(*args): ...
 
 
-# TODO: update commands...
 COMMANDS: list[Command] = [
-    # group config setters
     Command(GROUP_CONFIG, TYPE_SETTERS, "led"),
     Command(GROUP_CONFIG, TYPE_SETTERS, "motion"),
     Command(GROUP_CONFIG, TYPE_SETTERS, "motion-timeout"),
     Command(GROUP_CONFIG, TYPE_SETTERS, "pwm-range"),
-    # group config getters
     Command(GROUP_CONFIG, TYPE_GETTERS, "led"),
     Command(GROUP_CONFIG, TYPE_GETTERS, "motion"),
     Command(GROUP_CONFIG, TYPE_GETTERS, "motion-timeout"),
     Command(GROUP_CONFIG, TYPE_GETTERS, "pwm-range"),
-    # group info getters
     Command(GROUP_INFO, TYPE_GETTERS, "temp"),
     Command(GROUP_INFO, TYPE_GETTERS, "disk-usage"),
     Command(GROUP_INFO, TYPE_GETTERS, "version"),
-    # TODO: group led setters
-    # TODO: group led getters
-    # TODO: group motion getters
-    # TODO: group motion events
+    Command(GROUP_LED, TYPE_SETTERS, "duty"),
+    Command(GROUP_LED, TYPE_GETTERS, "duty"),
+    Command(GROUP_MOTION, TYPE_GETTERS, "duty"),
+    Command(GROUP_MOTION, TYPE_EVENTS, "watch"),
 ]
 
 
