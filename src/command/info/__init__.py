d54c7963877e991bb0f7fb6dea5d27f044991619
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-05 23:12:37 +0100

    add info commands

--- src/command/info/__init__.py
+++ src/command/info/__init__.py
@@ -3,4 +3,13 @@ from ..response import Response
 __all__ = ["run_getter"]
 
 
+def get_temp() -> float: ...
+
+
+def get_disk_usage() -> dict[str, int]: ...
+
+
+def get_version() -> str: ...
+
+
 def run_getter(id: int, command: str, *args) -> Response: ...
