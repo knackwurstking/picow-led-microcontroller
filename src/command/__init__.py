beb054c732cfbb35f76147f2c89b326e75cbdb41
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-05 11:14:25 +0100

    run commands logic

--- src/command/__init__.py
+++ src/command/__init__.py
@@ -3,47 +3,18 @@ from dataclasses import dataclass
 from . import config, info, led, motion
 
 __all__ = [
-    "COMMANDS",
     "run",
 ]
 
 
-TYPE_GETTERS = "get"
-TYPE_SETTERS = "set"
-TYPE_EVENTS = "event"
+TYPE_GETTER = "get"
+TYPE_SETTER = "set"
+TYPE_EVENT = "event"
 
 GROUP_CONFIG = "config"
 GROUP_INFO = "info"
 GROUP_LED = "led"
 GROUP_MOTION = "motion"
-
-
-@dataclass
-class Command:
-    group: str
-    type: str
-    command: str
-
-    def run(*args): ...
-
-
-COMMANDS: list[Command] = [
-    Command(GROUP_CONFIG, TYPE_SETTERS, "led"),
-    Command(GROUP_CONFIG, TYPE_SETTERS, "motion"),
-    Command(GROUP_CONFIG, TYPE_SETTERS, "motion-timeout"),
-    Command(GROUP_CONFIG, TYPE_SETTERS, "pwm-range"),
-    Command(GROUP_CONFIG, TYPE_GETTERS, "led"),
-    Command(GROUP_CONFIG, TYPE_GETTERS, "motion"),
-    Command(GROUP_CONFIG, TYPE_GETTERS, "motion-timeout"),
-    Command(GROUP_CONFIG, TYPE_GETTERS, "pwm-range"),
-    Command(GROUP_INFO, TYPE_GETTERS, "temp"),
-    Command(GROUP_INFO, TYPE_GETTERS, "disk-usage"),
-    Command(GROUP_INFO, TYPE_GETTERS, "version"),
-    Command(GROUP_LED, TYPE_SETTERS, "duty"),
-    Command(GROUP_LED, TYPE_GETTERS, "duty"),
-    Command(GROUP_MOTION, TYPE_GETTERS, "duty"),
-    Command(GROUP_MOTION, TYPE_EVENTS, "watch"),
-]
 
 
 @dataclass
@@ -53,6 +24,59 @@ class Response:
     data: any
 
 
+@dataclass
+class Command:
+    id: int
+    group: str
+    type: str
+    command: str
+
+    def run(self, *args) -> Response:
+        response = Response(0, None, None)
+
+        if self.group == GROUP_CONFIG:
+            if self.type == TYPE_SETTER:
+                response = config.run_setter(self.command, *args)
+            elif self.type == TYPE_GETTER:
+                response = config.run_getter(self.command, *args)
+            elif self.type == TYPE_EVENT:
+                response = config.run_event(self.command, *args)
+            else:
+                response.error = f'Type "{self.type}"not found!'
+
+            return response
+
+        if self.group == GROUP_INFO:
+            if self.type == TYPE_GETTER:
+                response = info.run_getter(self.command, *args)
+            else:
+                response.error = f'Type "{self.type}"not found!'
+
+            return response
+
+        if self.group == GROUP_LED:
+            if self.type == TYPE_SETTER:
+                response = led.run_setter(self.command, *args)
+            elif self.type == TYPE_GETTER:
+                response = led.run_getter(self.command, *args)
+            else:
+                response.error = f'Type "{self.type}"not found!'
+
+            return response
+
+        if self.group == GROUP_MOTION:
+            if self.type == TYPE_GETTER:
+                response = motion.run_getter(self.command, *args)
+            elif self.type == TYPE_EVENT:
+                response = motion.run_event(self.command, *args)
+            else:
+                response.error = f'Type "{self.type}"not found!'
+
+            return response
+
+        response.error = f'Type "{self.group}"not found!'
+        return response
+
+
 def run(id: int, group: str, type: str, command: str, *args) -> Response:
-    # TODO: iter commands and run, return response with/without error and data
-    ...
+    return Command(id, group, type, command).run()
