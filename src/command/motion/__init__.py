1fb1379f980eb3532ff45d16ef9c9b4d9d9da4f2
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-06 09:52:48 +0100

    add motion commands

--- src/command/motion/__init__.py
+++ src/command/motion/__init__.py
@@ -3,6 +3,12 @@ from ..response import Response
 __all__ = ["run_getter", "run_event"]
 
 
+def get_last_motion() -> int: ...
+
+
+def event_watch_motions(host: str, port: int): ...
+
+
 def run_getter(id: int, command: str, *args) -> Response: ...
 
 
