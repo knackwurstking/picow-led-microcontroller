8b61271bc31cfd05f0a06541398f6ca399c65230
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-06 09:35:09 +0100

    add led commands

--- src/command/led/__init__.py
+++ src/command/led/__init__.py
@@ -1,6 +1,14 @@
+from typing import Union
+
 from ..response import Response
 
 __all__ = ["run_setter", "run_getter"]
+
+
+def set_duty(duty_cycle: int, pin: Union[None, int] = None) -> None: ...
+
+
+def get_duty(*pins: int) -> int | None: ...
 
 
 def run_setter(id: int, command: str, *args) -> Response: ...
