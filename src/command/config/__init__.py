494dc05b4c84284b908fedcdc9c54ec294fb995a
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-07 15:17:15 +0100

    complete all config commands and ad args validation

--- src/command/config/__init__.py
+++ src/command/config/__init__.py
@@ -6,6 +6,13 @@ __all__ = ["run_setter", "run_getter"]
 
 
 def set_led_pins(*pins: int) -> None:
+    for pin in list(pins):
+        if not isinstance(pin, int):
+            raise Exception(f"all pins need to be from type {type(int)}")
+
+        if pin < 0:
+            raise Exception("led pins have to be positive")
+
     gp.led.set_pins(*pins)
 
 
@@ -14,6 +21,12 @@ def get_led_pins() -> list[int]:
 
 
 def set_motion_pin(pin: int) -> None:
+    if not isinstance(pin, int):
+        raise Exception(f"pin needs to be from type {type(int)}")
+
+    if pin < -1:
+        pin = -1
+
     gp.motion.set_pin(pin)
 
 
@@ -22,6 +35,9 @@ def get_motion_pin() -> int:
 
 
 def set_motion_timeout_value(timeout: int) -> None:
+    if not isinstance(timeout, int):
+        raise Exception(f"timeout needs to be from type {type(int)}")
+
     gp.motion.set_motion_timeout(timeout)
 
 
@@ -29,10 +45,15 @@ def get_motion_timeout_value() -> int:
     return gp.motion.get_motion_timeout()
 
 
-def set_pwm_range(min: int, max: int) -> None: ...
+def set_pwm_range(min: int, max: int) -> None:
+    if not isinstance(min, int) or not isinstance(max, int):
+        raise Exception(f"min/max needs to be from type {type(int)}")
+
+    gp.led.set_pwm_range(min, max)
 
 
-def get_pwm_range() -> list[int, int]: ...
+def get_pwm_range() -> list[int, int]:
+    return gp.led.get_pwm_range()
 
 
 def run_setter(id: int, command: str, *args) -> dc.Response:
