1e1787fbe98a732f124884d8c666a976366b0a15
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-07 15:37:14 +0100

    add getter/setter to led commands

--- src/command/led/__init__.py
+++ src/command/led/__init__.py
@@ -11,7 +11,39 @@ def set_duty(duty_cycle: int, pin: int) -> None: ...
 def get_duty(*pins: int) -> int | None: ...
 
 
-def run_setter(id: int, command: str, *args) -> dc.Response: ...
+def run_setter(id: int, command: str, *args) -> dc.Response:
+    response = dc.Response(id, None, None)
+
+    run_command = None
+
+    if command == "duty":
+        run_command = set_duty
+    else:
+        response.error = f'Command "{command}" not found!'
+        return response
+
+    try:
+        response.data = run_command(*args)
+    except Exception as ex:
+        response.error = str(ex)
+
+    return response
 
 
-def run_getter(id: int, command: str, *args) -> dc.Response: ...
+def run_getter(id: int, command: str, *args) -> dc.Response:
+    response = dc.Response(id, None, None)
+
+    run_command = None
+
+    if command == "duty":
+        run_command = get_duty
+    else:
+        response.error = f'Command "{command}" not found!'
+        return response
+
+    try:
+        response.data = run_command(*args)
+    except Exception as ex:
+        response.error = str(ex)
+
+    return response
