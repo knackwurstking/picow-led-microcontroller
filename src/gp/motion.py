911559979e0babbb07020db0570f61ccc3b70190
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-07 13:27:11 +0100

    Add motion_timeout to gp.motion

--- src/gp/motion.py
+++ src/gp/motion.py
@@ -12,15 +12,25 @@ onchange: Callable[[int], None] | None = None
 
 class GpMotion:
     _pin: int  # NOTE: use -1 to unset pin
+    _motion_timeout: int  # NOTE: resets the timer
 
-    def __init__(self, pin) -> None:
+    def __init__(self, pin: int = -1, motion_timeout: int = 60000) -> None:
         self._pin = pin
+        self._motion_timeout = motion_timeout
 
     def set_pin(self, pin: int) -> GpMotion:
         """Set the motion sensor Gp Pin, disable with -1"""
+        # TODO: update local config (save to pico)
         self._pin = pin
         return self
 
     def get_pin(self) -> int:
         """Returns the current motion sensor pin in use (-1 means its disabled)"""
         return self.pin
+
+    def set_motion_timeout(self, value: int) -> None:
+        # TODO: update local config (save to pico)
+        self.set_motion_timeout = value
+
+    def get_motion_timeout(self, value: int) -> int:
+        return self._motion_timeout
