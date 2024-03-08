b07f95930b6cc6670ae93f73d53cc6818ed1ec67
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-08 16:31:49 +0100

    add get_duty command

--- src/command/led/__init__.py
+++ src/command/led/__init__.py
@@ -1,6 +1,7 @@
 from typing import Callable
 
 import dc
+import gp
 
 __all__ = ["run"]
 
@@ -8,10 +9,25 @@ __all__ = ["run"]
 def set_duty(duty_cycle: int, pin: int | None = None) -> None:
     """Change the LED brightness (optional specify a specific pin,
     if not all pins are used)"""
-    ...
+    if pin is None or pin < 0:
+        # TODO: set duty for all pins
+        ...
+    else:
+        # TODO: set duty for a specific pin
+        ...
 
 
-def get_duty(*pins: int) -> int | None: ...
+def get_duty(pin: int | None = None) -> list[int] | int | None:
+    if pin is None:
+        return gp.led.get_duty()
+
+    if pin < 0:
+        return gp.led.get_duty()
+
+    if gp.led.has_pin(pin):
+        return gp.led.get_pin_duty(pin)
+
+    return None
 
 
 def run(id: int, _type: str, command: str, *args) -> dc.Response:
