19789c4437b1eea04792d2bf4eeca794727a0756
Udo Bauer <knackwurstking.tux@gmail.com>
2024-08-20 14:58:03 +0200

    Load from cache

--- src/gp/__init__.py
+++ src/gp/__init__.py
@@ -2,8 +2,13 @@ from . import led as _led
 
 
 def _init_led() -> _led.GpLED:
-    global led
-    led = _led.GpLED()  # TODO: set cached pins
+    led = _led.GpLED()
+
+    cache = led.cache.read()
+    if isinstance(cache, dict):
+        if "pins" in cache:
+            led.set_pins(*cache.pins)
+
     return led
 
 
