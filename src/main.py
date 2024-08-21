f6697759d333716792c3aa570b820231a2b927d2
Udo Bauer <knackwurstking.tux@gmail.com>
2024-08-21 16:41:23 +0200

    Main loop + server

--- src/main.py
+++ src/main.py
@@ -1,5 +1,79 @@
+from socket import socket, SOL_SOCKET, SO_REUSEADDR
+from time import sleep
+from sys import stderr
+
+import machine
+import network
+
+from secrets import PASS, SSID
+from constants import HOST, PORT, END_BYTE
+
+
 def main():
-    pass
+    setup()
+
+    server = socket()
+    server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen(5)
+
+    try:
+        while True:
+            print(f'[INFO] Waiting for client on "{HOST}:{PORT}"', file=stderr)
+            client, address = server.accept()
+            print(f'[INFO] Connected client from "{address}"', file=stderr)
+
+            data = bytes()
+            while True:
+                chunk = client.recv(1)
+                if chunk == END_BYTE:
+                    break
+
+                if chunk:
+                    data += chunk
+                else:
+                    break
+
+            if len(data) == 0:
+                client.close()
+                continue
+
+            # TODO: Parse client data, run, try to send response(?)
+            #   - Convert data to json
+            ...
+
+    finally:
+        disable_led()
+
+
+def setup():
+    disable_led()
+
+    sta_if = network.WLAN(network.STA_IF)
+
+    if not sta_if.isconnected():
+        sta_if.active(True)
+
+        print("[DEBUG] Try to connect...", file=stderr)
+        sta_if.connect(SSID, PASS)
+
+        while not sta_if.isconnected():
+            print("[DEBUG] ...wait for connection...", file=stderr)
+            sleep(0.25)
+
+        enable_led()
+
+    print(f"[DEBUG] ...ifconfig={sta_if.ifconfig()}", file=stderr)
+
+
+def enable_led():
+    led = machine.Pin("LED", machine.Pin.OUT)
+    led.on()
+
+
+def disable_led():
+    led = machine.Pin("LED", machine.Pin.OUT)
+    led.off()
 
 
 if __name__ == "__main__":
